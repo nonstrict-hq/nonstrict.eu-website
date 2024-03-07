180cd9a8a45174acee44feb1b2a812dc527c045f
Mathijs Kadijk <mkadijk@gmail.com>
2024-03-07 22:20:05 +0100

    Make sure to fetch new recordkit docs asset

--- recordkit-reference-swift/build.sh
+++ recordkit-reference-swift/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-rm -rf dist
-gh release download -R nonstrict-hq/RecordKit -p "recordkit-docs-*" -O ./recordkit-docs.zip
+rm -rf recordkit-docs.zip dist
+gh release download -R nonstrict-hq/RecordKit -p "recordkit-docs-*" -O ./recordkit-docs.zip --clobber
 unzip -o ./recordkit-docs.zip -d ./dist
