efad4d01e719377ae9b014aedeb1c9eda68015d5
Mathijs Kadijk <mkadijk@gmail.com>
2024-03-08 19:53:13 +0100

    Update repository name

--- recordkit-reference-electron/build.sh
+++ recordkit-reference-electron/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 rm -rf recordkit-docs-electron.zip dist
-gh release download -R nonstrict-hq/RecordKit -p "recordkit-docs-electron-*" -O ./recordkit-docs-electron.zip --clobber
+gh release download -R nonstrict-hq/RecordKit.Internal -p "recordkit-docs-electron-*" -O ./recordkit-docs-electron.zip --clobber
 unzip -o ./recordkit-docs-electron.zip -d ./dist
--- recordkit-reference-swift/build.sh
+++ recordkit-reference-swift/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 rm -rf recordkit-docs-swift.zip dist
-gh release download -R nonstrict-hq/RecordKit -p "recordkit-docs-swift-*" -O ./recordkit-docs-swift.zip --clobber
+gh release download -R nonstrict-hq/RecordKit.Internal -p "recordkit-docs-swift-*" -O ./recordkit-docs-swift.zip --clobber
 unzip -o ./recordkit-docs-swift.zip -d ./dist
