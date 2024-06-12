f5c9670f0069cf0414f7eb1e70f561a8024b857a
Tom Lokhorst <tom@lokhorst.eu>
2024-06-12 11:01:06 +0200

    Change WWDC Index to use pre-generated zip file from latest release

---
+++ wwdcindex-reference/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x -e
+rm -rf wwdcindex.zip dist
+gh release download -R nonstrict-hq/WWDCIndex.Internal -p "wwdcindex-*" -O ./wwdcindex.zip --clobber
+unzip -o ./wwdcindex.zip -d ./dist
+rm wwdcindex.zip
