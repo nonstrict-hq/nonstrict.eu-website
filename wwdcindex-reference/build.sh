0cd2db54ecc34bc242bde5cde5476103437dea53
Mathijs Kadijk <mkadijk@gmail.com>
2025-09-15 17:08:36 +0200

    Update build script to use curl for downloads

--- wwdcindex-reference/build.sh
+++ wwdcindex-reference/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x -e
 rm -rf wwdcindex.zip dist
-gh release download -R nonstrict-hq/WWDCIndex.Internal -p "wwdcindex-*" -O ./wwdcindex.zip --clobber
+
+# Workaround GitHub release download outage
+#gh release download -R nonstrict-hq/WWDCIndex.Internal -p "wwdcindex-*" -O ./wwdcindex.zip --clobber
+curl -L -o ./wwdcindex.zip https://static.nonstrict.eu/wwdcindex-2.77.zip
+
 unzip -o ./wwdcindex.zip -d ./dist
 rm wwdcindex.zip
