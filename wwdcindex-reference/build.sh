804ca1248496ffb3b120a2a24df27509c49697cd
Mathijs Kadijk <mkadijk@gmail.com>
2025-09-24 13:50:15 +0200

    Revert temporary workaround for GitHub release downloads outage

--- wwdcindex-reference/build.sh
+++ wwdcindex-reference/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x -e
 rm -rf wwdcindex.zip dist
 
-# Workaround GitHub release download outage
-#gh release download -R nonstrict-hq/WWDCIndex.Internal -p "wwdcindex-*" -O ./wwdcindex.zip --clobber
-curl -L -o ./wwdcindex.zip https://static.nonstrict.eu/wwdcindex-2.77.zip
+gh release download -R nonstrict-hq/WWDCIndex.Internal -p "wwdcindex-*" -O ./wwdcindex.zip --clobber
 
 unzip -o ./wwdcindex.zip -d ./dist
 rm wwdcindex.zip
